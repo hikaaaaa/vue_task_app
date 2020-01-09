33dd8e803512255287664d0fdf2325bae637e30d
hikaaaaa <i.hikaru.12225@gmail.com>
2020-01-09 18:13:15 +0900

    showページ作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,29 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+  
+  def destroy
+
   end
 end
