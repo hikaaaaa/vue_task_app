7486a2bc711cb227e03e3e54901662b71a2fc273
hikaaaaa <i.hikaru.12225@gmail.com>
2020-01-14 20:16:33 +0900

    fix a bit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i(show edit update destroy)
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -17,30 +18,32 @@ class TasksController < ApplicationController
       redirect_to tasks_path
     else
       render :new
-  end
+    end
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_params)
       redirect_to tasks_path
     else
       render :edit
     end
   end
-  
+
   def destroy
-    @task = Task.find(params[:id])
-    @task.destroy
+    @task.destroy!
     redirect_to tasks_path
   end
 
   private
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  end
   def task_params
     params.require(:task).permit(:title, :description)
   end
